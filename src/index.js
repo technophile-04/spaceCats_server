19a62e3c7300de8c8221a0d219dad2c2f0fa0182
Shiv Bhonde <shivbhonde04@gmail.com>
2022-07-30 16:25:15 +0530

    update start server script

--- src/index.js
+++ src/index.js
@@ -24,20 +24,23 @@ const mocks = {
 	}),
 };
 
-const server = new ApolloServer({
-	typeDefs,
-	resolvers,
-	dataSources: () => {
-		return {
-			trackAPI: new TrackAPI(),
-		};
-	},
-});
+async function startApolloServer(typeDefs, resolvers) {
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		dataSources: () => {
+			return {
+				trackAPI: new TrackAPI(),
+			};
+		},
+	});
 
-server.listen({ port: process.env.PORT || 4000 }).then(() => {
+	const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
 	console.log(`
-      🚀  Server is running!
-      🔉  Listening on port 4000
-      📭  Query at http://localhost:4000
-    `);
-});
+			🚀  Server is running
+			🔉  Listening on port ${port}
+			📭  Query at ${url}
+		  `);
+}
+
+startApolloServer(typeDefs, resolvers);
